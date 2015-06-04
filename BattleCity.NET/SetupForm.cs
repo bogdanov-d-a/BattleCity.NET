f8a22d5a8c0770b5604d1bc92074325fefbaee59
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2015-06-04 06:09:50 +0300

    Add dll works

--- BattleCity.NET/SetupForm.cs
+++ BattleCity.NET/SetupForm.cs
@@ -19,7 +19,26 @@ namespace BattleCity.NET
 
         private void addButton_Click(object sender, EventArgs e)
         {
-            DialogResult result = openFileDialog.ShowDialog();
+            openFileDialog.ShowDialog();
+            foreach (string fileName in openFileDialog.FileNames)
+            {
+                if (!ListHasItem(fileName))
+                {
+                    dllListBox.Items.Add(fileName);
+                }
+            }
+        }
+
+        private bool ListHasItem(string item)
+        {
+            foreach (string curItem in dllListBox.Items)
+            {
+                if (curItem == item)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
